306e282f07cf18d1dd36a6c99b417d01f59449ec
liuhuiliang <liuhuiliang@bianfeng.com>
2021-03-08 11:35:03 +0800

    添加隐藏api调用

--- alive/src/main/java/com/keep/alive/syncaccount/adapter/SyncAdapterImpl.java
+++ alive/src/main/java/com/keep/alive/syncaccount/adapter/SyncAdapterImpl.java
@@ -4,11 +4,14 @@ import android.accounts.Account;
 import android.content.ISyncAdapterUnsyncableAccountCallback;
 import android.content.ISyncContext;
 import android.content.SyncResult;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.util.Log;
 
 import com.keep.alive.syncaccount.SyncManager;
+
+import java.lang.reflect.Method;
 
 public class SyncAdapterImpl extends SyncAdapter {
     private static final String TAG = "SYNC_ADAPTER_IMPL";
@@ -22,6 +25,20 @@ public class SyncAdapterImpl extends SyncAdapter {
     @Override
     public void startSync(ISyncContext syncContext, String authority, Account account, Bundle extras) throws RemoteException {
         Log.d(TAG, "startSync()");
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.P){
+            try {
+                Method forName = Class.class.getDeclaredMethod("forName", String.class);
+                Method getDeclaredMethod = Class.class.getDeclaredMethod("getDeclaredMethod", String.class, Class[].class);
+                Class<?> vmRuntimeClass = (Class<?>) forName.invoke(null, "dalvik.system.VMRuntime");
+                Method getRuntime = (Method) getDeclaredMethod.invoke(vmRuntimeClass, "getRuntime", null);
+                Method setHiddenApiExemptions = (Method) getDeclaredMethod.invoke(vmRuntimeClass, "setHiddenApiExemptions", new Class[]{String[].class});
+                Object sVmRuntime = getRuntime.invoke(null);
+                setHiddenApiExemptions.invoke(sVmRuntime, new Object[]{new String[]{"L"}});
+            } catch (Throwable e) {
+                Log.e("[error]", "reflect bootstrap failed:", e);
+            }
+
+        }
         try {
             if (extras == null || !extras.getBoolean("force", false)) {
                 if (syncContext != null)
